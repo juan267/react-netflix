f9efdf96695b9be7af7c1fcb17774adfe1d0119c
juan267 <j.gomez267@gmail.com>
2016-03-31 23:11:50 -0500

    fix showcard

--- js/showCard.jsx
+++ js/showCard.jsx
@@ -2,14 +2,16 @@ const React = require('react')
 const { Link } = require('react-router')
 
 const ShowCard = (props) => (
-  <div className='show-card'>
-    <img className='show-card-img' src={`public/img/posters/${props.poster}`} />
-    <div className='show-card-text'>
-      <Link to={`/details/${props.imdbID}`}><h3 className='show-card-title'>{props.title}</h3></Link>
-      <h4 className='show-card-year'>({props.year})</h4>
-      <p className='show-card-description'>{props.description}</p>
+  <Link to={`/details/${props.imdbID}`}>
+    <div className='show-card'>
+      <img className='show-card-img' src={`public/img/posters/${props.poster}`} />
+      <div className='show-card-text'>
+        <h3 className='show-card-title'>{props.title}</h3>
+        <h4 className='show-card-year'>({props.year})</h4>
+        <p className='show-card-description'>{props.description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 const { string } = React.PropTypes
